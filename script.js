b32c83f8d20e07cd4ae2c9d4756b13b25e76af26
Karine.J <karine.jamet203@gmail.com>
2015-12-15 11:31:44 +0100

    mix paperjs et javascript

--- script.js
+++ script.js
@@ -1,27 +1,108 @@
-	$( document ).ready(function(){
-		$(".new").on("click",function() {
-			$(".newGame").hide();
-			console.log("hello");
-			$('canvas').attr('id','myCanvas');
-		});
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	$(document).ready(function() {
+
+
+	  /*   Click sur nouvelle partie   */
+	  $(".new").on("click", function() {
+	    /*   Cacher le bouton nouvelle partie   */
+	    $(".newGame").hide();
+	    console.log("hello");
+	    /*   Appel au canvas  */
+	    var canvas = document.getElementById('myCanvas');
+	    paper.setup(canvas);
+
+
+	    with(paper){/*   Faire les fonction du jeux ici avec paper.  */
+
+				/*Les objets*/
+
+				/* PacMan */
+				var path = new Path.Circle({
+					center: view.center,
+					radius: 30,
+					strokeColor: 'black'
+				});
+
+
+
+
+
+
+
+				/*Pommes*/
+
+
+
+
+
+
+
+
+
+
+
+				/*enemis*/
+
+
+
+
+
+
+
+
+				/* Fonction  */
+
+
+				/* Resize */
+				function onResize(event) {
+					// Whenever the window is resized, recenter the path:
+					path.position = view.center;
+				}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	    }
+
+	  });
 	});
