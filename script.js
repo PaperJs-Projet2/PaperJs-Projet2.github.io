0226e285dec308fb308cfd56cbafe8af41ace51a
Karine.J <karine.jamet203@gmail.com>
2015-12-15 14:22:48 +0100

    cercle qui suit la souris

--- script.js
+++ script.js
@@ -1,56 +1,49 @@
 paper.install(window);
 window.onload = function() {
 
-	$(document).ready(function() {
+  $(document).ready(function() {
 
 
-	  /*   Click sur nouvelle partie   */
-	  $(".new").on("click", function() {
-	    /*   Cacher le bouton nouvelle partie   */
-	    $(".newGame").hide();
-	    console.log("hello");
-	    /*   Appel au canvas  */
-	    var canvas = document.getElementById('myCanvas');
-	    paper.setup(canvas);
+    /*   Click sur nouvelle partie   */
+    $(".new").on("click", function() {
+      /*   Cacher le bouton nouvelle partie   */
+      $(".newGame").hide();
+			$.getScript("paper.js", function(){});
 
 
-	     /*   Faire les fonction du jeux ici avec paper.  */
-
-	      /*Les objets*/
-
-				var tool = new Tool();
+      /*   Appel au canvas  */
+      var canvas = document.getElementById('myCanvas');
+      paper.setup(canvas);
 
 
-	      /* PacMan */
-	      var path = new Path.Circle({
-	        center: view.center,
-	        radius: 30,
-	        strokeColor: 'red'
-	      });
+      /*   Faire les fonction du jeux ici avec paper.  */
+
+      /*Les objets*/
+
+      var tool = new Tool();
+			var tool2 = new Tool();
 
 
-
-	      /*Pommes*/
-
-				var pointx = Math.random()*$( "canvas" ).width();
-				var pointy = Math.random()*$( "canvas" ).height();
-
-	      var carre = new Path.Rectangle({
-	        point: [pointx,pointy],
-	        size: [15, 15],
-	        strokeColor: 'black',
-	        fillColor: 'black' // Si option de bonus, faire des couleurs aléatoire
-	      });
+      /* PacMan */
+      var pac = new Path.Circle({
+        center: view.center,
+        radius: 30,
+        strokeColor: 'red'
+      });
 
 
 
+      /*Pommes*/
 
+      var pointx = Math.random() * $("canvas").width();
+      var pointy = Math.random() * $("canvas").height();
 
-
-
-
-
-	      /*enemis*/
+      var carre = new Path.Rectangle({
+        point: [pointx, pointy],
+        size: [15, 15],
+        strokeColor: 'black',
+        fillColor: 'black' // Si option de bonus, faire des couleurs aléatoire
+      });
 
 
 
@@ -59,20 +52,43 @@ window.onload = function() {
 
 
 
-	      /* Fonction  */
 
-
-	      /* Resize */
-	      function onResize(event) {
-	        // Whenever the window is resized, recenter the path:
-	        path.position = view.center;
-					carre.position = Point.random() * view.size;
-	      }
+      /*enemis*/
 
 
 
 
-	  });
-	});
+
+
+
+
+      /* Fonction  */
+			console.log(pac.position.y);
+
+      tool.onMouseMove = function(event) {
+
+        var destination = event.point; //detecter la position de la mouse
+
+				var pointRecX = pac.position.x;
+				var pointRecY = pac.position.y;
+
+				pac.position.x = destination.x;
+				pac.position.y = destination.y;
+
+      }
+
+
+
+      /* Resize */
+      tool2.onResize = function(event) {
+        // Whenever the window is resized, recenter the path:
+        pac.position = view.center;
+      }
+
+
+
+
+    });
+  });
 
 }
