36e982eb279d1ea5cdb8b901063b55d6561eecf2
Horsell <ihorsell@gmail.com>
2015-12-15 16:54:27 +0100

    Ajout du raster Pacman

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
 paper.install(window);
 window.onload = function() {
 
-  $(document).ready(function() {
+	$(document).ready(function() {
 
 
-    /*   Click sur nouvelle partie   */
-    $(".new").on("click", function() {
-      /*   Cacher le bouton nouvelle partie   */
-      $(".newGame").hide();
+		/*   Click sur nouvelle partie   */
+		$(".new").on("click", function() {
+			/*   Cacher le bouton nouvelle partie   */
+			$(".newGame").hide();
 
 
 
-      /*   Appel au canvas  */
-      var canvas = document.getElementById('myCanvas');
-      paper.setup(canvas);
+			/*   Appel au canvas  */
+			var canvas = document.getElementById('myCanvas');
+			paper.setup(canvas);
 
 
-      /*   Faire les fonction du jeux ici avec paper.  */
+			/*   Faire les fonction du jeux ici avec paper.  */
 
-      /*Les objets*/
+			/*Les objets*/
 
-      var tool = new Tool();
+			var tool = new Tool();
 			var tool2 = new Tool();
 
 
-      /* PacMan */
-      var pac = new Path.Circle({
-        center: view.center,
-        radius: 30,
-        strokeColor: 'red'
-      });
+			/* PacMan */
+			var pac = new Path.Circle({
+				center: view.center,
+				radius: 30,
+				strokeColor: 'red'
+			});
+
+			//photo //
+			var pac = new Raster('test');
+			var loaded = false;
+
+			pac.on('load', function() {
+				loaded = true;
+
+			});
 
 
+			/*Pommes*/
+
+			var pointx = Math.random() * $("canvas").width();
+			var pointy = Math.random() * $("canvas").height();
 
 
-      /*Pommes*/
-
-      var pointx = Math.random() * $("canvas").width();
-      var pointy = Math.random() * $("canvas").height();
-
-
-      var carre = new Path.Rectangle({
-        point: [pointx, pointy],
-        size: [15, 15],
-        strokeColor: 'black',
+			var carre = new Path.Rectangle({
+				point: [pointx, pointy],
+				size: [15, 15],
+				strokeColor: 'black',
         fillColor: 'black' // Si option de bonus, faire des couleurs aléatoire
-      });
+    });
 
 
 
@@ -54,57 +61,57 @@ window.onload = function() {
 
 
 
-      /*enemis*/
-      var pointx = Math.random() * $("canvas").width();
-      var pointy = Math.random() * $("canvas").height();
+			/*enemis*/
+			var pointx = Math.random() * $("canvas").width();
+			var pointy = Math.random() * $("canvas").height();
 
 
-      var triangle = new Path.RegularPolygon(new Point(10, pointy), 3, 15)
-      triangle.fillColor = 'red';
-      triangle.rotate(90);
+			var triangle = new Path.RegularPolygon(new Point(10, pointy), 3, 15)
+			triangle.fillColor = 'red';
+			triangle.rotate(90);
 
 
-      triangle.onFrame = function(event) {
+			triangle.onFrame = function(event) {
 
-        this.position.x += 6;
-
-
-      }
+				this.position.x += 6;
 
 
-
+			}
 
 
 
-      tool.onMouseMove = function(event) {
+
+
+
+			tool.onMouseMove = function(event) {
 
         var destination = event.point; //detecter la position de la mouse
 
-				var pointRecX = pac.position.x;
-				var pointRecY = pac.position.y;
+        var pointRecX = pac.position.x;
+        var pointRecY = pac.position.y;
 
-				var newX = pointRecX+((destination.x-pointRecX)/100);
-				var newY = pointRecY+((destination.y-pointRecY)/100);
+        var newX = pointRecX+((destination.x-pointRecX)/100);
+        var newY = pointRecY+((destination.y-pointRecY)/100);
 
-				pac.position.x = newX;
-			 	pac.position.y = newY;
-
-
-      }
+        pac.position.x = newX;
+        pac.position.y = newY;
 
 
+    }
 
-      /* Resize */
-      tool2.onResize = function(event) {
+
+
+    /* Resize */
+    tool2.onResize = function(event) {
         // Whenever the window is resized, recenter the path:
         pac.position = view.center;
 
-      }
+    }
 
 
 
 
-    });
-  });
+});
+});
 
 }
